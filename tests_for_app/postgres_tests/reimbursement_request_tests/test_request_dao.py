c68966d14d4e65fdfb2bdb29f39b849fac3ae8a7
amandue10 <amandue10.ag@gmail.com>
2021-12-15 08:03:24 -0700

    end of night push

--- tests_for_app/postgres_tests/reimbursement_request_tests/test_request_dao.py
+++ tests_for_app/postgres_tests/reimbursement_request_tests/test_request_dao.py
@@ -12,5 +12,10 @@ def test_create_request_success():
 
 
 def test_select_all_requests_by_employee_id():
-    initial_request = request_dao.get_reimbursement_request_by_id(1)
+    initial_request = request_dao.get_reimbursement_requests_by_id(1)
     assert initial_request.employee_id == 1
+
+
+def test_select_all_rr_success():
+    rr = request_dao.get_all_reimbursement_history()
+    assert len(rr) >= 1
