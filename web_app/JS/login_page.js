e94d790f9c467daa0bd27b0e5ef58d7bddc22865
amandue10 <amandue10.ag@gmail.com>
2021-12-16 14:08:38 -0700

    login page html, js, with errors

---
+++ web_app/JS/login_page.js
@@ -0,0 +1,25 @@
+const employeeId = document.getElementById("idInput")
+const password = document.getElementById("passwordInput")
+const firstName = document.getElementById("firstNameInput")
+const lastName = document.getElementById("lastNameInput")
+
+async function employeeLogin(){
+    let response = await fetch(
+        " http://127.0.0.1:5000/employee_login", {
+            method:"POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({"employeeId":employeeId.value, "firstName":firstName.value, "lastName":lastName.value, "password":password.value})
+        }
+    )
+    if (response.status === 200){
+        let body = await response.json()
+        if (body["validated"]){
+            sessionStorage.setItem("validated", true)
+            window.location.href = "employee_page.html"
+        } else {
+            alert("Employee login failed: please try again")
+        }
+    } else {
+        alert("your login request failed")
+    }
+}
